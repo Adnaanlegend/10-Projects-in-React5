5bf91f0a755dc0df37fdabba8e2db8fc4bae9755
Adnaan Hussain <92253624+Adnaanlegend@users.noreply.github.com>
2023-08-02 23:27:53 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="h-[80px] gap-2 bg-white my-4 rounded-lg flex justify-center items-center text-xl font-medium">
+    <div className="h-[80px] gap-2 bg-rose-400 my-4 rounded-lg flex justify-center items-center text-xl font-medium">
       <img src="/logo.svg" alt="Logo of our App" />
       <h1>Contacts App</h1>
     </div>
